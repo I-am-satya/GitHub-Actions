5830e5f0c5675e939853daa80e7d7e1a6bc02630
Satya_venkat_kurella <satyavenkat.kurella@gmail.com>
2025-10-31 12:06:01 +0530

    Remove test_home function from app.py
    Removed test_home function from app.py.

--- src/app.py
+++ src/app.py
@@ -1,10 +1,4 @@
 from flask import Flask
-from app import app
-
-def test_home():
-    response = app.test_client().get('/')
-    assert response.status_code == 200
-    assert response.data == b"Hello, Jenkins!"
 
 app = Flask(__name__)
 
