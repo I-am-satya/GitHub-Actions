9d4eb629847df25449d491ce538a275a098ed1a4
Satya_venkat_kurella <satyavenkat.kurella@gmail.com>
2025-11-06 14:18:38 +0530

    added name and mail to generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -1,11 +1,16 @@
+from faker import Faker
 import numpy as np
 import pandas as pd
-
+import re
 
 def generate_synthetic_data(num_samples=1000, seed=42):
   np.random.seed(seed)
+  faker=Faker()
+  Faker.seed(seed)
   # Generate synthetic data
-
+  names = [faker.name() for _ in range(num_samples)]
+  emails = [name.lower().replace(" ",".")+"@infosys.com" for name in names]
+  phones = [faker.phone_number() for _ in range(num_samples)]
   ages = np.random.randint(18, 70, size=num_samples)
   
   income = np.random.normal(loc=50000, scale=15000, size=num_samples).astype(int)
@@ -18,11 +23,12 @@ def generate_synthetic_data(num_samples=1000, seed=42):
   #Create DataFrame
 
   df = pd.DataFrame({
-  
-  'Age': ages,
-  'Income': income, 
-  'Purchased': purchased
-
+    'Name': names,
+    'Email':emails,
+    'Phone Number': phones,
+    'Age': ages,
+    'Income': income, 
+    'Purchased': purchased
   })
   return df
 
