dda56a4d9539f2960a8b9247bd9db6daa13145c8
Satya_venkat_kurella <satyavenkat.kurella@gmail.com>
2025-11-07 15:22:14 +0530

    Added date and time addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,7 +1,12 @@
+from datetime import datetime
 #addition
+now = datetime.now()
+print("Date and time when test case run ")
+print(now.strftime("%Y-%m-%d %H:%M:%S))
+                   
 def add(a, b):
     return a + b
 
 def test_add():
-    assert add(1, 2) == 6
+    assert add(1, 2) == 3
     assert add(1, -1) == 0
